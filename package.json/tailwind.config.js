1edbc2181695f258c3416a50a308c5070213897d
joneidson <joneidsonvictorjc@gmail.com>
2025-08-04 05:24:07 -0300

    Feature:alteração de cores do site

---
+++ package.json/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  darkMode: 'class', // ativa o modo dark via classe 'dark'
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // onde procurar as classes tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
